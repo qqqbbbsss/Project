a81f615dba765fb88902f34c646fc2efc397985b
user <user@DESKTOP-FVOUIA7>
2024-09-10 22:55:21 +0300

    Добавьте файлы проекта.

---
+++ Source.c
@@ -0,0 +1,23 @@
+#include <locale.h>
+#include <stdio.h>
+void main()
+{
+	puts("           ");
+	puts("          ");
+	puts("                              @         @");
+	puts("                  @@         @ @       @ @");
+	puts("                 @          @   @     @   @");
+	puts("                @@         @     @@@@@     @");
+	puts("                @@@       @                 @");
+	puts("                 @@@      @   ()        ()  @");
+	puts("                   @@@     @       >*<      @");
+	puts("                     @@@@   @              @");
+	puts("                      @   @@  @          @");
+	puts("                     @       @            @");
+	puts("                     @      @              @");
+	puts("                      @  @@@@              @");
+	puts("                      @  @   @            @");
+	puts("                       @@    @    @@@@    @");
+	puts("                             @    @  @    @");
+	puts("                               @@     @@");
+}
